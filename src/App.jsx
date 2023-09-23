7a934037fcd9ee5030d7d21d24940e8eecc507ab
Franyusd Gomez Bolivar <analistacost@gmail.com>
2023-09-23 15:09:53 -0500

    se termino la aplicacion

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import Crud from "./components/Crud"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Crud />
+    </>
+  )
+}
+
+export default App
